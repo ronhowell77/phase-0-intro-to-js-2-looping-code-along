fb99fe161c945960a1fbb6e85c88f50c86115013
Ronnie <ron.howell0326@gmail.com>
2022-10-05 14:31:48 -0400

    commit tree c1b25079a2e976bbb084ceae52617e7484da5251

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+const { interfaces } = require("mocha");
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+const event = 'surprise';
+
+function writeCards(names, event){
+  let newArray = [] 
+  for (let i = 0; i < names.length; i++ ){
+      newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);  
+    }
+    return newArray
+}
+function countDown(number){
+    let i=10;
+    while (i >= 0){
+        console.log(i);
+        i--
+    }
+    return number;
+}
